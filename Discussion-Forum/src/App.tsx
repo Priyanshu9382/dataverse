d0198a028020dc042b2de8d0f6d085d478c30f64
Priyanshu9382 <priyanshumishra419@gmail.com>
2025-02-11 14:37:13 +0530

    fixed some issues for hosting

--- Discussion-Forum/src/App.tsx
+++ Discussion-Forum/src/App.tsx
@@ -1,29 +1,31 @@
-import {Routes, Route} from 'react-router-dom'
-import Question from './pages/Question'
-import Landing from './pages/Landing'
-import Popular from './pages/Popular'
-import Categories from './pages/Categories'
-import Category from './pages/Category'
-import { RecoilRoot } from 'recoil'
-import Browse from './pages/Browse'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Question from './pages/Question';
+import Landing from './pages/Landing';
+import Popular from './pages/Popular';
+import Categories from './pages/Categories';
+import Category from './pages/Category';
+import { RecoilRoot } from 'recoil';
+import Browse from './pages/Browse';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
+function App() {
   return (
     <RecoilRoot>
-      <Routes>
-        <Route path='/' element={<Landing />} />
-        <Route path='/question/:id' element={<Question />} />
-        <Route path='/popular' element={<Popular />} />
-        <Route path='/categories' element={<Categories />} />
-        <Route path='/categories/:id' element={<Category />} />
-        <Route path='/browse/:id' element={<Browse />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
+      <Router basename="/dataverse">  {/* Fix: Use BrowserRouter */}
+        <Routes>
+          <Route path='/' element={<Landing />} />
+          <Route path='/question/:id' element={<Question />} />
+          <Route path='/popular' element={<Popular />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/categories/:id' element={<Category />} />
+          <Route path='/browse/:id' element={<Browse />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+      </Router>
     </RecoilRoot>
-  )
+  );
 }
 
-export default App
+export default App;
