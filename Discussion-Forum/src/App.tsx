e74ffdd3fbc8341c6fa714fa7a408a6fb0239389
Priyanshu Mishra <priyanshumishra419@gmail.com>
2025-02-15 10:31:28 +0530

    Update App.tsx

--- Discussion-Forum/src/App.tsx
+++ Discussion-Forum/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  Routes, Route } from 'react-router-dom';
 import Question from './pages/Question';
 import Landing from './pages/Landing';
 import Popular from './pages/Popular';
